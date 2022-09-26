2da324746f5be5f0882d5d52a1321fd5714c113c
Hendradito Dwi A <hendradito21@gmail.com>
2022-09-26 22:02:18 +0700

    update

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -2,14 +2,41 @@
 // https://mui.com/material-ui/react-text-field/#basic-textfield
 // dan Card
 // https://mui.com/material-ui/react-card/#basic-card
+import {
+  Card,
+  CardContent,
+  Box,
+  TextField,
+  Typography,
+  CardActions,
+  Button,
+} from '@mui/material';
+import { padding } from '@mui/system';
 
 const ContactForm = () => {
-    // Form berisi name, phone, email, dan photo url
-    // Buatlah state newContact berupa objek sesuai dengan data yang ada
+  // Form berisi name, phone, email, dan photo url
+  // Buatlah state newContact berupa objek sesuai dengan data yang ada
 
-    return (
-        <> </>
-    );
-}
+  return (
+    <>
+      <Card sx={{ margin: 'auto', width: '400px' }}>
+        <CardContent sx={{ padding: '50px' }}>
+          <TextField fullWidth required id="Name" label="Name"></TextField>
+          <TextField fullWidth required id="Phone" label="Phone"></TextField>
+          <TextField fullWidth required id="Email" label="Email"></TextField>
+          <TextField
+            fullWidth
+            required
+            id="Photo"
+            label="Photo Url"
+          ></TextField>
+          <CardActions>
+            <Button size="small">Add New</Button>
+          </CardActions>
+        </CardContent>
+      </Card>
+    </>
+  );
+};
 
 export default ContactForm;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,20 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 // atau langsung tambahkan dengan sx={{}}
 import React from 'react';
+import { Divider } from '@mui/material';
+import Typography from '@mui/material/Typography';
 
 // Header berisi judul dan subtitle
 // Judul: Call a Friend
 // Subtitle: Your friendly contact app
 const Header = () => {
   return (
-    <></>
+    <>
+      <Typography variant="h2" gutterBottom>
+        Call a Friend
+      </Typography>
+      <Divider>Your friendly contact app</Divider>
+    </>
   );
 };
 
