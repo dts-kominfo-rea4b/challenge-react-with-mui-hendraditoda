d38794306bcb7a187bbf993a1e00f659144de1d1
Hendradito Dwi A <hendradito21@gmail.com>
2022-09-29 22:42:30 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
+import { Grid } from '@mui/material';
 import './App.css';
+import Contact from './components/Contact';
 import ContactForm from './components/ContactForm';
 import Header from './components/Header';
 
 // Uncomment untuk memuat daftar kontak
 import contactsJSON from './data/contacts.json';
-const App = () => {
+const App = (contacts) => {
   // Masukkan Header dan lakukan map untuk Contact ke dalam div App
   // untuk membuat daftar kontak bisa menggunakan MUI list
   // https://mui.com/material-ui/react-list/#folder-list
 
   // Masukkan contacts yang sudah didapat dalam JSON sebagai initial state
   // Buatlah handler untuk menambahkan kontak baru yang akan dikirim ke ContactForm
+  const handleNewContact = () => {
+    // const valueInput = input.value;
+    // const list = document.createElement('li');
+    // list.append(valueInput);
+    // output.appendChild(list);
+    // input.value = '';
+  };
 
   return (
     <div className="App">
       <Header />
-      <ContactForm />
+      <Grid container spacing={2}>
+        <Grid item xs="auto" md="auto">
+          <ContactForm handleNewContact={handleNewContact} />
+        </Grid>
+        <Grid item xs="auto" md="auto">
+          {contacts.map((contact, index) => (
+            <Contact data={contact} key={index} />
+          ))}
+        </Grid>
+      </Grid>
     </div>
   );
 };
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,34 @@
 // Terima props lalu tampilkan dalam Contact component
 // Contact component dapat berupa MUI ListItem
 // https://mui.com/material-ui/react-list/#folder-list
+import {
+  Avatar,
+  Card,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+} from '@mui/material';
 import React from 'react';
 
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 // atau langsung tambahkan dengan sx={{}}
-const Contact = ({ data }) => {
-    // Contact berisi foto, nama, telepon, dan email
-    return (<></>);
+const Contact = ({ contact }) => {
+  // Contact berisi foto, nama, telepon, dan email
+  return (
+    <Card sx={{ minWidth: 'auto' }}>
+      <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'backgroundg.paper' }}>
+        <ListItem>
+          <ListItemAvatar>
+            <Avatar src={contact.photo} />
+            <ListItemText primary={contact.name}></ListItemText>
+            <ListItemText primary={contact.phone}></ListItemText>
+            <ListItemText primary={contact.email}></ListItemText>
+          </ListItemAvatar>
+        </ListItem>
+      </List>
+    </Card>
+  );
 };
 
 export default Contact;
